4c2986fc868fb461492ff0fc42e78635f926b618
Rafal Sroka <rafal.sroka.it@gmail.com>
2017-11-16 22:03:08 +0100

    Download and parse price data

--- CryptoTicker/ViewController.swift
+++ CryptoTicker/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var valueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let url = URL(string:"https://blockchain.info/ticker")!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil else {
+                DispatchQueue.main.async {
+                    self.valueLabel.text = "error"
+                }
+                return
+            }
+
+            guard let data = data else {
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            let tickerList = try! decoder.decode(TickerList.self, from: data)
+
+            DispatchQueue.main.async {
+                self.valueLabel.text = String(format: "%@%.0f", tickerList.eur.currencySymbol, tickerList.eur.recentMarketPrice)
+            }
+        }.resume()
     }
 
     override func didReceiveMemoryWarning() {
